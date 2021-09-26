01939c4e757fdeb23bf435e17a6591ee64cc5649
Shreyans Mehta <shreyans.mehta@somaiya.edu>
2021-09-26 22:44:08 +0530

    background update

--- frontend/Kyusei.py
+++ frontend/Kyusei.py
@@ -4,18 +4,34 @@ import pandas as pd
 import time
 from PIL import Image
 
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1445905595283-21f8ae8a33d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80");
+background-size: cover;
+}
+</style>
+'''
+
 st.markdown("""
 <style>
 .color-font {
     font-size:100px !important;
     color: "red";
 }
+.color-font2 {
+    font-size:30px !important;
+    color: "white";
+    
+}
 </style>
 """, unsafe_allow_html=True)
-st.markdown('<p class="color-font">Kyusei</p>', unsafe_allow_html=True)
+st.markdown('<p class="color-font"><font color = "white">Kyusei</font></p>',
+            unsafe_allow_html=True)
 
 
-st.subheader("Repositories")
+st.markdown('<p class="color-font2" ><font color = "white">Repositories</font></p>',
+            unsafe_allow_html=True)
 # for i in range(5):
 repo_names = ['requests', 'scrapy', 'fastapi', 'rich', 'textual', 'httpx', 'pydantic', 'tqdm', 'bokeh', 'sanic', 'dash', 'mangadex', 'pyseto', 'Slicer', 'pytago', 'earthquakes',
               'dvc', 'aiospotify', 'ray', 'ruck', 'quimb', 'fileseq', 'Lean', 'merk', 'pe', 'Ignition', 'Cura', 'nextpnr', 'onlineshop', 'labyrinth', 'sentry', 'flask', 'httpie']
